ada5ad541df9eacfad3baccb0239fda8dcc16be7
Yana Kirdyai <kirdyai@ukr.net>
2022-10-01 22:50:53 +0300

    js

---
+++ src/js/api-service.js
@@ -0,0 +1,7 @@
+// import axios from "axios";
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '30297842-30e01d12e48588937048bb7ce';
+// const image_type = 'photo';
+// const orientation = 'horizontal';
+// const safesearch = 'true';
